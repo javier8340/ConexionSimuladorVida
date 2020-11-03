00e0ba5a3b39425b367015d34db7f6a1b91be358
javier <jgg8340@gmail.com>
2020-11-03 16:22:24 +0100

    Fix errores en consola

--- src/main/java/com/Javiergg/SimuladorVida/Eventos/ClickSign.java
+++ src/main/java/com/Javiergg/SimuladorVida/Eventos/ClickSign.java
@@ -14,7 +14,12 @@ public class ClickSign implements Listener {
     @EventHandler
     public void clickSign(PlayerInteractEvent event){
         Player player = event.getPlayer();
-        Material mat = event.getClickedBlock().getType();
+        Material mat;
+        try {
+             mat = event.getClickedBlock().getType();
+        }catch(Exception e){
+             mat = Material.AIR;
+        }
         if (mat.equals(Material.ACACIA_SIGN) || mat.equals(Material.SPRUCE_SIGN) || mat.equals(Material.BIRCH_SIGN) ||
                 mat.equals(Material.CRIMSON_SIGN) || mat.equals(Material.DARK_OAK_SIGN) || mat.equals(Material.JUNGLE_SIGN)
                 || mat.equals(Material.OAK_SIGN) || mat.equals(Material.WARPED_SIGN) || mat.equals(Material.ACACIA_WALL_SIGN)
